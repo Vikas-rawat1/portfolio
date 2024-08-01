0ac6f0e884a4a6364943cbd6d7b209b27e053b73
vikas <rawatvikas689@gmail.com>
2024-08-01 23:20:45 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProjectTab from "./components/ProjectTab";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div>
       <Router>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<About />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/projectTab" element={<ProjectTab />} />
         </Routes>
-        <Footer/>
+      <Footer />
       </Router>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/ProjectTab.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Tilt } from "react-tilt";
+
+function ProjectTab() {
+  const defaultOptions = {
+    reverse: false,
+    max: 35,
+    perspective: 1000,
+    scale: 1.1,
+    speed: 1000,
+    transition: true,
+    axis: null,
+    reset: true,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+  };
+
+  const projects = [
+    {
+      imageUrl: "Images/works/anima.jpg",
+      projectLink: "https://vikas-rawat1.github.io/anima/",
+      description:
+        "A simple landing page with animation showcasing modern web design principles and engaging visuals.",
+    },
+    {
+      imageUrl: "Images/works/1.jpg",
+      projectLink: "https://daz3d-clone.netlify.app",
+      description:
+        "A UI clone of Daz3D featuring 3D model displays, product listings, user account management, and a shopping cart, all designed with React for a seamless user experience.",
+    },
+    {
+      imageUrl: "Images/works/clothing.jpg",
+      projectLink: "https://vikas-rawat1.github.io/clothing_landing_page/",
+      description:
+        "A stylish clothing brand landing page built with HTML, CSS, and JavaScript, effectively showcasing brand aesthetics and product offerings.",
+    },
+    {
+      imageUrl: "Images/works/obys.jpg",
+      projectLink: "https://obysreplica.netlify.app/",
+      description:
+        "Dynamic website for a fictional agency with engaging animations and modern design, built with HTML, CSS, JavaScript, and GSAP.",
+    },
+    {
+      imageUrl: "Images/works/cinecraze.jpg",
+      projectLink: "https://cinecraze.netlify.app",
+      description:
+        "A movie discovery tool with features like search, detailed information, and watchlists, designed with React, Redux, and Tailwind CSS to enhance user experience and accessibility.",
+    },
+    {
+      imageUrl: "Images/works/roastery.jpg",
+      projectLink: "https://roasterycoffee.netlify.app/",
+      description:
+        "A reimagined coffee website featuring modern design elements, interactive components, and rich media content for a captivating experience.",
+    },
+  ];
+
+  const ProjectThumbnail = ({
+    imageUrl,
+    projectLink,
+    githubLink,
+    description,
+  }) => (
+    <Tilt
+      options={defaultOptions}
+      style={{ height: 300, width: 300 }}
+      className="m-auto mb-20 md:m-0 md:mb-20 lg:mr-10"
+    >
+      <div className="relative">
+        <img
+          src={imageUrl}
+          alt="Thumbnail"
+          className="w-full object-cover h-72 fit"
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-between bg-gray-800 rounded-md p-4 opacity-0 hover:opacity-100 transition-opacity duration-300">
+          <p className="text-white text-lg mb-4">{description}</p>
+          <div className="flex justify-center">
+            <button className="bg-emerald-500 hover:bg-emerald-600 text-white font-semibold px-4 py-2 rounded-md">
+              <Link to={projectLink} className="mt-10">View Project</Link>
+            </button>
+            {/* {githubLink && (
+              <button className="bg-gray-700 hover:bg-gray-800 text-white font-semibold px-4 py-2 rounded-md ml-4">
+                <Link to={githubLink}>GitHub Repo</Link>
+              </button>
+            )} */}
+          </div>
+        </div>
+      </div>
+    </Tilt>
+  );
+
+  return (
+    <div className="bg-black">
+      <p className="text-3xl text-center text-white">My Works</p>
+      <div className="mt-20 lg:flex lg:flex-wrap justify-center">
+        {projects.map((project, index) => (
+          <ProjectThumbnail
+            key={index}
+            imageUrl={project.imageUrl}
+            projectLink={project.projectLink}
+            githubLink={project.githubLink}
+            description={project.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProjectTab;
