f6e8ff3b97601ec5e15a789e7ae38da374273a35
vikas <rawatvikas689@gmail.com>
2024-08-17 19:30:11 +0530

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
       setFormData({ name: "", email: "", message: "" });
     }
   }; dfgdf
-dfgsdfsd
+dfgsdf
   return (
     <>
       <div className="bg-black text-white mt-20">
